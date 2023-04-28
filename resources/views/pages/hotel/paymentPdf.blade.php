a0824a4c2612174a200a6d114087abc983beb407
Barnoch Anas <anasbarnoch@gmail.com>
2023-04-28 12:13:24 +0100

    HMR-12 generate receipt payment pdf

--- resources/views/pages/hotel/paymentPdf.blade.php
+++ resources/views/pages/hotel/paymentPdf.blade.php
@@ -1,2 +1,4 @@
 <p>Name: {{ $booking->user->name }}</p>
 <p>Hotel: {{ $booking->hotel->name }}</p>
+<p>City: {{ $booking->hotel->city->name }}</p>
+<p>Total: {{ $booking->room->price_per_night }} DH</p>
